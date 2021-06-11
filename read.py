12cbc721338e06ee898ba85ed35678f04171106f
FanChiang Yu-Hua <yuhua.fanchiang@gmail.com>
2021-06-11 14:17:15 +0800

    add word count function

--- read.py
+++ read.py
@@ -1,20 +1,67 @@
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
-		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
 print('檔案讀取完了, 總共有', len(data), '筆資料')
 
 sum_len = 0
 for d in data:
-	sum_len = sum_len + len(d)
+    sum_len = sum_len + len(d)
 print('留言的平均為', sum_len/len(data))
+
 new = []
 for d in data:
-	if len(d) < 100:
-		new.append(d)
+    if len(d) < 100:
+        new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有', len(good), '筆留言提到good')
+
+# 文字計數
+wc = {}
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
+
+for word in wc:
+    if wc[word] > 1000000:
+         print(word, wc[word])
+
+print(len(wc))
+print(wc['Eva'])
+
+while True:
+    word = input('請問你想查甚麼字: ')
+    if word == 'q':
+        break
+    if word in wc:
+    	print(word, '出現次數為: ', wc[word])
+    else:
+    	print('這個詞沒有出現過喔!')
+
+print('感謝使用本查詢功能!')
+
+
+
+
+
+
+
+
+
+
+
+
